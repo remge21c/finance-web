a3724deff8663068f5a42bba3cc146132ddf3444
remge21c <remge@naver.com>
2026-04-25 08:24:02 +0900

    fix: manifest purpose 타입 에러 수정

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,7 +12,7 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: 'portrait',
     icons: [
       { src: '/icon-192x192.png', sizes: '192x192', type: 'image/png' },
-      { src: '/icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' },
+      { src: '/icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
     ],
   }
 }
